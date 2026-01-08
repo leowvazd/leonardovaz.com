8cea0d17c614d851d71d5dfba44b87d5991f4390
leowvazd <87207693+leowvazd@users.noreply.github.com>
2026-01-08 12:57:33 -0300

    First commit
    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    fontSize: {
+      base: "3rem",
+    },
+    extend: {},
+    screens: {
+      sm: "640px",
+      msm: "600px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+};
